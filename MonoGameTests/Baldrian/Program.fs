8381246af679c67feed885ef0149e370bf32c605
ahef <alexander.heffner@cirromed.de>
2024-12-24 16:32:58 +0100

    feat: Initial commit with F# MonoGame project

---
+++ MonoGameTests/Baldrian/Program.fs
@@ -0,0 +1,38 @@
+﻿namespace Baldrian
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+module Game =
+    type BaldrianGame() as this =
+        inherit Game()
+
+        let graphics = new GraphicsDeviceManager(this)
+        let mutable spriteBatch: SpriteBatch = null
+
+        do
+            this.Content.RootDirectory <- "Content"
+            this.IsMouseVisible <- true
+
+        override this.Initialize() =
+
+            base.Initialize()
+
+        override this.LoadContent() =
+            spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+            base.LoadContent()
+
+        override this.Update(gameTime) =
+            
+            base.Update(gameTime)
+
+        override this.Draw(gameTime) =
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime)
+    
+    [<EntryPoint>]
+    let main args =
+        let game = new BaldrianGame()
+        game.Run ()
+        0
